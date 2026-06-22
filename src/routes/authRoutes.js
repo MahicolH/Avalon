f081616047ed2cabddc04ddbf4d08cb3f37ba4de
Mahicol <167448515+MahicolH@users.noreply.github.com>
2026-06-21 22:07:50 -0500

    Update authRoutes.js

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -2,7 +2,19 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
 
+// LOGIN
 router.post('/login', authController.login);
-router.post('/register', authController.register); 
+
+// REGISTRO
+router.post('/register', authController.register);
+
+// USUARIOS PENDIENTES
+router.get('/pending-users', authController.pendingUsers);
+
+// APROBAR USUARIO
+router.put('/approve-user/:id', authController.approveUser);
+
+// RECHAZAR USUARIO
+router.delete('/reject-user/:id', authController.rejectUser);
 
 module.exports = router;
